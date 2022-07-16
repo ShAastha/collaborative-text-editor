0bcaae28703949f8e7985ebab939bbc6346c5b22
Sh-Aastha <sharmaaa@tcd.ie>
2022-07-17 04:19:50 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const server = require('http').Server(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3001;
-
+app.use(cors());
 app.use(express.static(path.join(__dirname + '../web/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../web/build/index.html'));
